7315748425e5fc38ebdbab6d393bf53420a3772f
Pauli Kauppinen <kaupppa@gmail.com>
2016-08-15 16:24:32 +0300

    Fix navbar buttons (chrome)
    The material buttons has wierd look on chrome, added own buttons.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'navbar',
   styles: [`
+    .nav-button {
+      background: transparent;
+      cursor: pointer;
+      cursor: hand;
+      outline: none;
+      border: none;
+      font-size: 14px;
+      font-family: Roboto, "Helvetica Neue", sans-serif;
+      font-weight: 500;
+      color: currentColor;
+      text-align: center;
+      margin: 0;
+      min-width: 88px;
+      line-height: 36px;
+      padding: 0 16px;
+      border-radius: 3px;
+    }
+
     .fill{
       flex: 1 1 auto;
     }
@@ -17,10 +35,12 @@ import { Component } from '@angular/core';
   `],
   template: `
     <md-toolbar color="navbar">
-      <button md-button [routerLinkActive]="['active-link']" routerLink="/"
-        [routerLinkActiveOptions]="{exact:true}">Etusivu</button>
-      <button md-button [routerLinkActive]="['active-link']" routerLink="/hinnasto">Hinnasto</button>
-      <button md-button [routerLinkActive]="['active-link']" routerLink="/saapuminen">Saapuminen</button>
+      <button class="nav-button" [routerLinkActive]="['active-link']"
+        routerLink="/" [routerLinkActiveOptions]="{exact:true}">Etusivu</button>
+      <button class="nav-button" [routerLinkActive]="['active-link']"
+        routerLink="/hinnasto">Hinnasto</button>
+      <button class="nav-button" [routerLinkActive]="['active-link']"
+        routerLink="/saapuminen">Saapuminen</button>
     </md-toolbar>
   `
 })
